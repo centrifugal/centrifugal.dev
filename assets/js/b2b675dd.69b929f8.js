71077fb4a6549975749c7ccd2abdac6c761e1dbd
Alexander Emelin <frvzmb@gmail.com>
2022-07-20 11:12:34 +0300

    Deploy website - based on fe6a86d2f8417afb033d1b684b9efd0d338b2baa

---
+++ assets/js/b2b675dd.69b929f8.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkcentrifugal_dev=self.webpackChunkcentrifugal_dev||[]).push([[533],{8017:function(e){e.exports=JSON.parse('{"permalink":"/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":9,"blogDescription":"Centrifugal Blog","blogTitle":"Centrifugal Blog"}')}}]);
