4bab638ba3daa0aa452048830daf8c1d77f84de4
Alexander Emelin <frvzmb@gmail.com>
2023-09-27 23:30:11 +0300

    Deploy website - based on ab19e668766f145148ac5a5042d8eeb8af4ae969

---
+++ install_pro.sh
@@ -0,0 +1,70 @@
+#!/bin/sh
+
+set -e
+
+if [ -n "${DEBUG}" ]; then
+  set -x
+fi
+
+_centrifugo_latest() {
+  curl -s https://api.github.com/repos/centrifugal/centrifugo-pro/releases/latest | grep "tag_name" | awk '{print $2}' | sed 's/[",]//g'
+}
+
+_detect_binary() {
+  os="$(uname)"
+  case "$os" in
+    Linux|Darwin) echo "centrifugo" ;;
+    *) echo "Unsupported operating system: $os" 1>&2; return 1 ;;
+  esac
+  unset os
+}
+
+_detect_os() {
+  os="$(uname)"
+  case "$os" in
+    Linux) echo "linux" ;;
+    Darwin) echo "darwin" ;;
+    *) echo "Unsupported operating system: $os" 1>&2; return 1 ;;
+  esac
+  unset os
+}
+
+_detect_arch() {
+  arch="$(uname -m)"
+  case "$arch" in
+    amd64|x86_64) echo "amd64" ;;
+    arm64|aarch64) echo "arm64" ;;
+    *) echo "Unsupported processor architecture: $arch" 1>&2; return 1 ;;
+  esac
+  unset arch
+}
+
+_download_url() {
+  echo "https://github.com/centrifugal/centrifugo-pro/releases/download/$CENTRIFUGO_VERSION/${centrifugoBinary}-pro_${CENTRIFUGO_VERSION#"v"}_${centrifugoOs}_${centrifugoArch}.tar.gz"
+}
+
+main() {
+  if [ -z "${CENTRIFUGO_VERSION}" ]; then
+    CENTRIFUGO_VERSION=$(_centrifugo_latest)
+  fi
+
+  centrifugoInstallPath=`pwd`
+  centrifugoBinary="$(_detect_binary)"
+  centrifugoOs="$(_detect_os)"
+  centrifugoArch="$(_detect_arch)"
+  centrifugoDownloadUrl="$(_download_url)"
+
+  mkdir -p -- "$centrifugoInstallPath"
+
+  echo "Downloading centrifugo from URL: $centrifugoDownloadUrl"
+
+  tmpdir=$(mktemp -d)
+  curl -sSLf "$centrifugoDownloadUrl" >"$tmpdir/centrifugo.tar.gz"
+  tar -xzf $tmpdir/centrifugo.tar.gz ${centrifugoBinary}
+  chmod +x "$centrifugoBinary"
+  rm -r $tmpdir
+
+  echo "centrifugo is now executable in $centrifugoInstallPath"
+}
+
+main
