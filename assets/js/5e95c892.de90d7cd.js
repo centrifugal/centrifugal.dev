2b0082945221efa05dfac0018a9122cb473a7662
Alexander Emelin <frvzmb@gmail.com>
2024-07-30 21:14:56 +0300

    Deploy website - based on eb185312b8321617443aac1ba9cfd9f53e2fb719

---
+++ assets/js/5e95c892.de90d7cd.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkcentrifugal_dev=self.webpackChunkcentrifugal_dev||[]).push([[9661],{93143:(e,r,s)=>{s.r(r),s.d(r,{default:()=>d});s(67294);var u=s(36905),a=s(62581),c=s(18015),n=s(18790),t=s(78299),l=s(85893);function d(e){return(0,l.jsx)(a.FG,{className:(0,u.Z)(c.k.wrapper.docsPages),children:(0,l.jsx)(t.Z,{children:(0,n.H)(e.route.routes)})})}}}]);
